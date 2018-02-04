073c9f491e4b453b8209cdc47bc8f4c51b7c01d7
Nishan D'Souza <nishand@gmail.com>
2018-02-04 13:00:01 -0800

    Roster/Add time forms

--- app/routes.py
+++ app/routes.py
@@ -6,8 +6,6 @@ from app import app
 from app.db import *
 import hashlib
 
-from flask import Markup
-
 hashed_pwd = '763e6715ab44cd899ae1b172cc78d5a7'
 
 @app.route('/')
@@ -22,13 +20,20 @@ def roster():
 		return render_template('login.html')
 	return render_template('roster.html', roster = getRoster())
 
+@app.route('/roster/add', methods=['POST'])
+def add_swimmer():
+	# add database code to add swimmer - see login for how to interact with form data
+	return render_template('roster.html', roster = getRoster())
+
 @app.route('/login', methods=['POST'])
 def login():
+	if not request:
+		abort(400)
 	if validate_pwd(request.form['password']) and request.form['username'] == 'admin':
 		session['logged_in'] = True
 	else:
 		flash('Incorrect password')
-	return index()	
+	return index()
 
 @app.route('/logout')
 def logout():
@@ -36,12 +41,25 @@ def logout():
     return index()
 
 @app.route('/times')
-def list_times():
+def list_all_times():
+	if not session.get('logged_in'):
+		return render_template('login.html')
+	# Database+template code for all times
 
-@app.route('/add')
-def add_time():    
+@app.route('/add') # Landing page for adding times
+def add_time_menu():
+	if not session.get('logged_in'):
+		return render_template('login.html')
+	return render_template('addTime.html', swimmers = getRoster())
+
+@app.route('/addTime', methods=['POST']) # Never direct link here w/o request data -> Form request destination for adding times
+def addTime():
+	if not request:
+		abort(400)
+	if not session.get('logged_in'):
+		return render_template('login.html')
+	# add database code to add swimmer time - see login for how to interact with form
+	return add_time_menu()
 
 def validate_pwd(password):
 	return hashlib.md5(password.encode()).hexdigest() == hashed_pwd
-
-	
