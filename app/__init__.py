d00b87fb4aa27853405950ae7d6593f33bef6846
Andrew Ke <andrew@andrewke.org>
2018-02-04 15:00:43 -0800

    Updates
    - Roster add works
    - disable_login mode added
    - Logout button added
    - @requiresauth decorator added

--- app/__init__.py
+++ app/__init__.py
@@ -3,5 +3,3 @@ from flask import Flask
 app = Flask(__name__)
 
 from app import routes
-
-print("Hi")
--- app/routes.py
+++ app/routes.py
@@ -1,65 +1,75 @@
 import sys
-
-
+from functools import wraps
 from flask import flash, redirect, render_template, request, session, abort
 from app import app
 from app.db import *
 import hashlib
 
 hashed_pwd = '763e6715ab44cd899ae1b172cc78d5a7'
+disable_login = False
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        if not session.get('logged_in') and not disable_login:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated
 
 @app.route('/')
+@requires_auth
 def index():
-	if not session.get('logged_in'):
-		return render_template('login.html')
-	return render_template('base.html')
+    return render_template('base.html')
 
-@app.route('/roster')
+@app.route('/roster', methods=['GET'])
+@requires_auth
 def roster():
-	if not session.get('logged_in'):
-		return render_template('login.html')
-	return render_template('roster.html', roster = getRoster())
+    return render_template('roster.html', roster = getRoster())
 
-@app.route('/roster/add', methods=['POST'])
+@app.route('/roster', methods=['POST'])
 def add_swimmer():
-	# add database code to add swimmer - see login for how to interact with form data
-	return render_template('roster.html', roster = getRoster())
+    # add database code to add swimmer - see login for how to interact with form data
+    addPlayer(name = request.form['name'])
+    return redirect("/roster")
+
+@app.route('/login', methods=['GET'])
+def login():
+    return render_template('login.html', hideLogout = True)
 
 @app.route('/login', methods=['POST'])
-def login():
-	if not request:
-		abort(400)
-	if validate_pwd(request.form['password']) and request.form['username'] == 'admin':
-		session['logged_in'] = True
-	else:
-		flash('Incorrect password')
-	return index()
+def auth():
+    if not request:
+        abort(400)
+    if validate_pwd(request.form['password']) and request.form['username'] == 'admin':
+        session['logged_in'] = True
+        return redirect("/roster")
+    else:
+        flash('Incorrect password')
+
 
 @app.route('/logout')
 def logout():
     session['logged_in'] = False
-    return index()
+    return redirect("/login")
 
 @app.route('/times')
+@requires_auth
 def list_all_times():
-	if not session.get('logged_in'):
-		return render_template('login.html')
-	# Database+template code for all times
+    # Database+template code for all times
+    pass
 
 @app.route('/add') # Landing page for adding times
+@requires_auth
 def add_time_menu():
-	if not session.get('logged_in'):
-		return render_template('login.html')
-	return render_template('addTime.html', swimmers = getRoster())
+    return render_template('addTime.html', swimmers = getRoster())
 
 @app.route('/addTime', methods=['POST']) # Never direct link here w/o request data -> Form request destination for adding times
+@requires_auth
 def addTime():
-	if not request:
-		abort(400)
-	if not session.get('logged_in'):
-		return render_template('login.html')
-	# add database code to add swimmer time - see login for how to interact with form
-	return add_time_menu()
+    if not request:
+        abort(400)
+    # add database code to add swimmer time - see login for how to interact with form
+    return add_time_menu()
 
 def validate_pwd(password):
-	return hashlib.md5(password.encode()).hexdigest() == hashed_pwd
+    return hashlib.md5(password.encode()).hexdigest() == hashed_pwd
